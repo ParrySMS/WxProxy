e7b03c794796ef91b2356ac0d5bad7ba3bcf40a9
ParrySMS <yh@szu.me>
2018-07-30 20:51:58 +0800

    update

--- config/config.php
+++ config/config.php
@@ -17,17 +17,16 @@ define("REDIRECT_URI",'');
 //限制的接口范围
 
 //OAUTH2微信网页授权
-define('ACTION_URL_OAUTH2',json_encode([
+define('ACTION_URL_OAUTH',json_encode([
     'oauth2AccessToken'=>'https://api.weixin.qq.com/sns/oauth2/access_token',
     'oauth2RefreshToken'=>'https://api.weixin.qq.com/sns/oauth2/refresh_token',
+    'oauth2Userinfo'=>' https://api.weixin.qq.com/sns/userinfo',
+    'oauth2IsVaildAccessToken'=>'https://api.weixin.qq.com/sns/auth',
 ]));
 
 //todo 按组拆分掉url
 define('ACTION_REGION','return array(
-            \'getSnsAccessToken\'=>\'\',
-            \'refreshWxToken\',
-            \'getWxUserinfo\',
-            
+             
             \'getAuthAccessToken\',
             
             \'sendWxMsgTemplate\',
--- forward.php
+++ forward.php
@@ -10,9 +10,12 @@ require "./func/http.php";
 require "./config/config.php";
 
 try {
+    //只使用post方法
     //获取参数
-    $url = isset($_GET['url']) ? $_GET['url'] : null;
+    $url = isset($_POST['url']) ? $_POST['url'] : null;
+    $data = isset($_POST) ? $_POST : [];
 
+    //todo post转发 地址url 数据data 
 
 } catch (Exception $e) {
     echo MSG_ERROR . $e->getMessage();
